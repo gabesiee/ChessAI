5fa783405d40c4ac150cba3251ade2a6779f2093
gabesiee <gabriel.gunullu@edu.esiee.fr>
2020-11-25 11:35:56 +0100

    empty board generation

---
+++ Assets/Scripts/GenerateBoard.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateBoard : MonoBehaviour
+{
+    [SerializeField] private GameObject chessboard;
+    [SerializeField] private GameObject lightCube;
+    [SerializeField] private GameObject darkCube;
+
+    private void Start()
+    {
+        generateEmptyBoard();
+    }
+
+    private void generateEmptyBoard()
+    {
+        String squareColor = "light";
+        for (int i = 0; i < 64; i++)
+        {
+            Vector3 pos = new Vector3(i / 8, 0, i % 8);
+
+            if (i % 8 == 0)
+            {
+                if (squareColor == "light")
+                {
+                    squareColor = "dark";
+                }
+                else
+                {
+                    squareColor = "light";
+                }
+            }
+
+            if (squareColor == "dark")
+            {
+                GameObject cube = Instantiate(darkCube, pos, Quaternion.identity) as GameObject;
+                cube.transform.parent = chessboard.transform;
+
+                squareColor = "light";
+            }
+            else
+            {
+                GameObject cube = Instantiate(lightCube, pos, Quaternion.identity) as GameObject;
+                cube.transform.parent = chessboard.transform;
+
+                squareColor = "dark";
+            }
+        }
+    }
+}
